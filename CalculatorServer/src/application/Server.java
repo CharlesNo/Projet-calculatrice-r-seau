e0d14d74179f3cebf2153ba8b11e86303a6f2758
CharlesNo <charles63500@gmail.com>
2014-01-23 09:05:05 +0100

    Server terminé + commentaires et Ajout de interrupt
    Signed-off-by: CharlesNo <charles63500@gmail.com>

--- CalculatorServer/src/application/Server.java
+++ CalculatorServer/src/application/Server.java
@@ -1,55 +1,139 @@
 /*---------------------------------------------------------------*/
-/** Fichier : Server.java
- *
+/**
+ * Fichier : Server.java
+ * 
  * créé le 23 janv. 2014 à 08:26:32
- *
+ * 
  * Auteurs : Léo Riera & Vincent Voyer
  */
 package application;
 
+import java.io.IOException;
 import java.net.ServerSocket;
 import java.util.ArrayList;
 import java.util.GregorianCalendar;
 import java.util.List;
-
+import java.util.Vector;
 import services.ClientInfo;
 
 /*---------------------------------------------------------------*/
 /**
+ * The Class Server.
+ * 
  * @author vivoyer
- *
  */
 public class Server implements Runnable
 {
-	private static final int port = 5042;
-	
-	private GregorianCalendar start;
-	
-	private List<ClientInfo> clients;
-	
-	private ServerSocket serverSocket;
-	
+	/** The Constant LISTENING_PORT. */
+	private static final int		LISTENING_PORT	= 5042;
+	/** The clients. */
+	private List<ClientInfo>		clients;
+	/** The server socket. */
+	private ServerSocket			serverSocket;
+	/** The start. */
+	private final GregorianCalendar	start;
+	/** The thread. */
+	private final Thread			thread;
+
+	/**
+	 * Instantiates a new server.
+	 */
 	public Server()
 	{
+		super();
 		start = new GregorianCalendar();
 		clients = new ArrayList<ClientInfo>();
-		
+		thread = new Thread(this);
+		clients = new Vector<>();
+		serverSocket = null;
+		try
+		{
+			serverSocket = new ServerSocket(LISTENING_PORT);
+			System.out.println("Server started on port " + LISTENING_PORT);
+		}
+		catch (final IOException se)
+		{
+			System.err.println("Can not start listening on port "
+					+ LISTENING_PORT);
+			se.printStackTrace();
+			System.exit(-1);
+		}
 	}
-	
-	/*---------------------------------------------------------------*/
+
+	/* _________________________________________________________ */
 	/**
+	 * Retourne la valeur du champ start.
+	 * 
+	 * @return la valeur du champ start.
+	 */
+	public final GregorianCalendar getStart()
+	{
+		return start;
+	}
+
+	/* _________________________________________________________ */
+	/**
+	 * Interrupt.
+	 * 
+	 * @see java.lang.Thread#interrupt()
+	 */
+	public void interrupt()
+	{
+		thread.interrupt();
+	}
+
+	/* _________________________________________________________ */
+	/**
+	 * Checks if is interrupted.
+	 * 
+	 * @return true, if is interrupted
+	 * @see java.lang.Thread#isInterrupted()
+	 */
+	public boolean isInterrupted()
+	{
+		return thread.isInterrupted();
+	}
+
+	/* _________________________________________________________ */
+	/**
+	 * Run.
+	 * 
 	 * @see java.lang.Runnable#run()
 	 */
 	@Override
 	public void run()
 	{
-		// PENSER à IMPLEMENTER Auto-generated method stub
-
+		while (!isInterrupted())
+		{
+			try
+			{
+				final ClientInfo client = new ClientInfo(serverSocket.accept());
+				clients.add(client);
+			}
+			catch (final IOException ioe)
+			{
+				ioe.printStackTrace();
+			}
+		}
+		for (final ClientInfo client : clients)
+		{
+			client.interrupt();
+		}
 	}
 
+	/* _________________________________________________________ */
+	/**
+	 * Start.
+	 * 
+	 * @see java.lang.Thread#start()
+	 */
+	public void start()
+	{
+		thread.start();
+	}
 }
-
-
 /*---------------------------------------------------------------*/
-/* Fin du fichier Server.java
-/*---------------------------------------------------------------*/
+/*
+ * Fin du fichier Server.java
+ * /*---------------------------------------------------------------
+ */
--- CalculatorServer/src/services/ClientInfo.java
+++ CalculatorServer/src/services/ClientInfo.java
@@ -1,8 +1,9 @@
 /*---------------------------------------------------------------*/
-/** Fichier : ClientInfo.java
- *
+/**
+ * Fichier : ClientInfo.java
+ * 
  * créé le 23 janv. 2014 à 08:26:59
- *
+ * 
  * Auteurs : Léo Riera & Vincent Voyer
  */
 package services;
@@ -13,13 +14,13 @@ import java.net.Socket;
 /*---------------------------------------------------------------*/
 /**
  * @author vivoyer
- *
+ * 
  */
 public class ClientInfo
 {
-	private final Socket socket;
-	
-	public ClientInfo(Socket socket) throws IOException
+	private final Socket	socket;
+
+	public ClientInfo(final Socket socket) throws IOException
 	{
 		this.socket = socket;
 	}
@@ -34,7 +35,7 @@ public class ClientInfo
 	{
 		final int prime = 31;
 		int result = 1;
-		result = prime * result + ((socket == null) ? 0 : socket.hashCode());
+		result = (prime * result) + ((socket == null) ? 0 : socket.hashCode());
 		return result;
 	}
 
@@ -45,26 +46,45 @@ public class ClientInfo
 	 * @see java.lang.Object#equals(java.lang.Object)
 	 */
 	@Override
-	public boolean equals(Object obj)
+	public boolean equals(final Object obj)
 	{
 		if (this == obj)
+		{
 			return true;
+		}
 		if (obj == null)
+		{
 			return false;
+		}
 		if (getClass() != obj.getClass())
+		{
 			return false;
-		ClientInfo other = (ClientInfo) obj;
+		}
+		final ClientInfo other = (ClientInfo) obj;
 		if (socket == null)
 		{
 			if (other.socket != null)
+			{
 				return false;
-		} else if (!socket.equals(other.socket))
+			}
+		}
+		else if (!socket.equals(other.socket))
+		{
 			return false;
+		}
 		return true;
 	}
+
+	/* _________________________________________________________ */
+	/**
+	 */
+	public void interrupt()
+	{
+		// TODO Auto-generated method stub
+	}
 }
-
-
 /*---------------------------------------------------------------*/
-/* Fin du fichier ClientInfo.java
-/*---------------------------------------------------------------*/
+/*
+ * Fin du fichier ClientInfo.java
+ * /*---------------------------------------------------------------
+ */
